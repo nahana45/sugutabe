c89f7a490b4ab3d8cb7382d8628bb17c1d3f3090
平田菜々 <hiratanana@hi-MacBook-Air.local>
2023-06-29 15:22:28 +0900

    first commit

---
+++ assets/js/kadai.js
@@ -0,0 +1,53 @@
+// JavaScript Document
+
+//ボタン開閉
+$("#openbtn").on("click",function(){
+  $(".navigation-2").toggleClass("open");
+})
+$("#closebtn").on("click",function(){
+  $(".navigation-2").removeClass("open");
+})
+$("#page-link a").on("click",function(){
+  $(".navigation-2").removeClass("open");
+})
+
+//リンクまでゆっくり移動
+$('#page-link a[href*="#"]').click(function () {
+	var elmHash = $(this).attr('href'); //ページ内リンクのHTMLタグhrefから、リンクされているエリアidの値を取得
+	var pos = $(elmHash).offset().top;	//idの上部の距離を取得
+	$('body,html').animate({scrollTop: pos}, 500); //取得した位置にスクロール。500の数値が大きくなるほどゆっくりスクロール
+	return false;
+});
+
+function slideAnime(){
+	//====左に動くアニメーションここから===
+		$('.leftAnime').each(function(){ 
+			var elemPos = $(this).offset().top-50;
+			var scroll = $(window).scrollTop();
+			var windowHeight = $(window).height();
+			if (scroll >= elemPos - windowHeight){
+				//左から右へ表示するクラスを付与
+				//テキスト要素を挟む親要素（左側）とテキスト要素を元位置でアニメーションをおこなう
+				$(this).addClass("slideAnimeLeftRight"); //要素を左枠外にへ移動しCSSアニメーションで左から元の位置に移動
+				$(this).children(".leftAnimeInner").addClass("slideAnimeRightLeft");  //子要素は親要素のアニメーションに影響されないように逆の指定をし元の位置をキープするアニメーションをおこなう
+			}else{
+				//左から右へ表示するクラスを取り除く
+				$(this).removeClass("slideAnimeLeftRight");
+				$(this).children(".leftAnimeInner").removeClass("slideAnimeRightLeft");
+				
+			}
+		});
+		
+	}
+	
+	// 画面をスクロールをしたら動かしたい場合の記述
+	$(window).scroll(function (){
+		slideAnime();/* アニメーション用の関数を呼ぶ*/
+	});// ここまで画面をスクロールをしたら動かしたい場合の記述
+
+	// 画面が読み込まれ
+
+
+
+
+
